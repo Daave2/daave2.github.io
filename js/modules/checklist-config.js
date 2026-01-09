1c5727f0e276b7116bba93daa51a2ee19a6cac47
Daave2 <131151314+Daave2@users.noreply.github.com>
2026-01-09 10:23:26 +0000

    Add checklist configuration module

---
+++ js/modules/checklist-config.js
@@ -0,0 +1,4 @@
+export const CHECKLIST_CONFIG = {
+  token: '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',
+  gistId: '95a61f781f9655c1abb90d6daff4a7c2'
+};
